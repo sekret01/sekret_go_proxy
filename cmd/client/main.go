72b01458ae83b4f9c223bcacba89dc3df5e2aeb9
Pavel <asinskijp188@gmail.com>
2026-09-01 11:16:27 +0300

    рефакторинг cmd/client/main

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"os"
 
 	"github.com/sekret01/sekret_go_proxy/internal/config"
 	"github.com/sekret01/sekret_go_proxy/internal/framers"
+	"github.com/sekret01/sekret_go_proxy/internal/utils"
 
 	"github.com/sekret01/sekret_go_proxy/internal/app"
 	"github.com/sekret01/sekret_go_proxy/internal/encryptors"
@@ -21,34 +21,55 @@ import (
 
 func main() {
 
+	hub := setupLogger()
+	hub.Info("START APP")
+
+	hub.Info("START LOAD CONFIG")
+	cfg := loadConfig(hub)
+
+	hub.Info("START REGISTRATE MODULS")
+	printRegistrates(hub)
+
+	hub.Info("START BUILD CLIENT-APP MODULS")
+	client := builClientApp(hub, cfg)
+
+	hub.Info("RUN CLIENT-APP")
+	client.Run()
+}
+
+func loadConfig(hub logger.LoggerHub) *config.Config {
+	cfg, err := config.LoadConfig("configs/client.example.yaml")
+	if err != nil {
+		hub.Critical("[loadConfig]: " + err.Error())
+		criticalExit()
+	}
+	return cfg
+}
+
+func setupLogger() logger.LoggerHub {
 	hub := logger.GetLoggerHub()
 	consoleLogger := loggers.NewConsoleLogger()
 	hub.Registrate(consoleLogger)
 	hub.SetLevel(logger.INFO)
+	return hub
+}
 
-	hub.Info("START IMPORTS")
-	cfg, err := config.LoadConfig("configs/client.example.yaml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	hub.Info("CONFIG NAS BEEN IMPORTED")
-
-	hub.Info("REGISTRATED MODULS")
-	printRegistrates(hub)
-
-	hub.Info("START BUILD ClientApp MODULS")
+func builClientApp(hub logger.LoggerHub, cfg *config.Config) app.ClientApp {
 	client, err := app.NewClientApp(cfg)
 	if err != nil {
-		log.Fatal(err)
+		hub.Critical("[loadConfig]: " + err.Error())
+		criticalExit()
 	}
-
-	hub.Info("RUN ClientApp")
-	client.Run()
+	return *client
 }
 
 func printRegistrates(hub logger.LoggerHub) {
-	hub.Debug("RUUUN")
-	fmt.Printf("Encryptors: %s\n", encryptors.List())
-	fmt.Printf("Transports: %s\n", transports.List())
-	fmt.Printf("Framers: %s\n", framers.List())
+	hub.Debug("List of egistrated moduls")
+	hub.Debug("Encryptors: " + utils.ListToString(encryptors.List()))
+	hub.Debug("Transports: " + utils.ListToString(transports.List()))
+	hub.Debug("Framers: " + utils.ListToString(framers.List()))
+}
+
+func criticalExit() {
+	os.Exit(1)
 }
